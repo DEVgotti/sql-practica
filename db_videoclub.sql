583a585b60640434c28ae6d2e7244a371fcbc835
Gotti <david.lmonras@gmail.com>
2024-02-05 21:56:54 +0100

    Añadido generos a películas

--- db_videoclub.sql
+++ db_videoclub.sql
@@ -65,19 +65,27 @@ foreign key (movie_id) references movie(movie_id);
 alter table genre_movie add constraint fk_genre_movie_movie
 foreign key (movie_id) references movie(movie_id);
 
+alter table genre add constraint unique_genre_name
+unique (genre_name);
+
 alter table genre_movie add constraint fk_genre_movie_genre
 foreign key (genre_id) references genre(genre_id);
 
 alter table movie add constraint fk_director_movie
 foreign key (director_id) references director(director_id);
 
-insert into genre (genre_name) 
-values 
+alter table director add constraint unique_director_name
+unique (director_name);
+
+insert into genre (genre_name)
+values
 ('Acción'),
 ('Drama'),
 ('Suspense'),
-('Bélico'),
-('Aventura');
+('Romance'),
+('Aventura'),
+('Misterio'),
+('Western');
 
 insert into director (director_name)
 values
@@ -101,6 +109,27 @@ values
 ('The Unforgiven', '1960-01-01', 4, 'In post-Civil War Texas, the Zachary and Rawlins families are intrinsically tied together. Ben Zachary, the head of the family following the death of his father, Will Zachary, in Ben being the oldest son, and patriarch Zeb Rawlins are partners in a cattle ranching operation. Zeb''s unassuming son Charlie Rawlins would like to court Ben''s younger sister, adopted Rachel Zachary - something that doesn''t quite sit well with Ben in he believing Charlie not worthy of Rachel - while Zeb''s daughter Georgia Rawlins would be happy with any of the Zachary brothers, although she has her sights set on middle son Cash Zachary. Their world is upset with the arrival of an older man brandishing a saber. It is not the saber which is concerning, but rather the story that he is spreading: that Rachel is of Indian heritage, most specifically Kiowa. Regardless of the truth, which matriarch Mattilda Zachary knows, this story places a strain on the community who don''t want a "dirty Injun" in their midst, threatens all the relationships between the Zacharys and Rawlins, and causes a division within the Zachary family, as the Zacharys try to protect themselves and their property against those who don''t want Rachel there, including the Kiowa who want Rachel, one of their own, back.', 2),
 ('The Sheepman', '1958-01-01', 5, 'A stranger in a Western cattle-town behaves with remarkable self-assurance, establishing himself as a man to be reckoned with. The reason appears with his stock: a herd of sheep, which he intends to graze on the range. The horrified inhabitants decide to run him out at all costs.', 3),
 ('Per un pugno di dollari', '1964-01-01', 6, 'With nothing but profit on his mind, a taciturn American with a lightning-fast right hand rides into San Miguel, a sun-bleached border town where everyone is either rich or dead. As the amoral man with no name discovers that the sleepy town provides the perfect opportunity to make a quick buck, the mysterious stranger soon finds himself caught in the middle of a destructive, long-standing feud. After all, the corrupt Baxters and the murderous Rojos, two feuding families fighting for control, won''t let a nameless nobody get a slice of the cake. But the dusty, poncho-clad newcomer has already set his sights on the grand prize--the only way to get it is to play the gangs off against each other in a deadly battle of wits. Of course, no one has pulled such a reckless stunt and lived to tell the tale. How far is a man willing to go for a fistful of dollars?', 5);
+
+insert into genre_movie (movie_id, genre_id)
+values
+(1, 1), -- Yojimbo
+(1, 2), -- Yojimbo
+(1, 3), -- Yojimbo
+(2, 1), -- Shichinin no samurai
+(2, 2), -- Shichinin no samurai
+(3, 1), -- Seppuku
+(3, 2), -- Seppuku
+(3, 6), -- Seppuku
+(4, 1), -- Jusan-nin no shikaku
+(4, 2), -- Jusan-nin no shikaku
+(4, 5), -- Jusan-nin no shikaku
+(5, 2), -- The Unforgiven
+(5, 4), -- The Unforgiven
+(5, 7), -- The Unforgiven
+(6, 7), -- The Sheepman
+(7, 1), -- Per un pugno di dollari
+(7, 2), -- Per un pugno di dollari
+(7, 7); -- Per un pugno di dollari
 
 insert into residence (residence_postcode, residence_street, residence_number, residence_floor, residence_extension)
 values

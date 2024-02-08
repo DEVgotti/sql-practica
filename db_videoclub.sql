12eb4566808def9f6324660f6a7cfe91cf48f3b6
Gotti <david.lmonras@gmail.com>
2024-02-08 22:55:35 +0100

    Cambiado 'movie_published' tipo date a tipo int para guardar solo el año

--- db_videoclub.sql
+++ db_videoclub.sql
@@ -31,7 +31,7 @@ create table if not exists rental(
 create table if not exists movie(
 	movie_id serial primary key,
 	movie_title varchar(200) not null,
-	movie_published date not null,
+	movie_published int not null,
 	director_id int not null,
 	movie_synopsis text not null,
 	movie_copies int not null
@@ -96,19 +96,15 @@ values
 ('George Marshall'),
 ('Sergio Leone');
 
-/*
- * Desconozco el mes/día de publicación de las películas así que utilizo el 1 de enero como relleno para respetar el formato fecha.
- * Sería más lógico utilizar un 'int' con el año pero no se si utilizar el tipo 'date' aquí forma parte de la practica como tal.
- */
 insert into movie (movie_title, movie_published, director_id, movie_synopsis, movie_copies)
 values
-('Yojimbo', '1961-01-01', 1, 'asd', 1),
-('Shichinin no samurai', '1954-01-01', 1, 'A veteran samurai, who has fallen on hard times, answers a village''s request for protection from bandits. He gathers 6 other samurai to help him, and they teach the townspeople how to defend themselves, and they supply the samurai with three small meals a day. The film culminates in a giant battle when 40 bandits attack the village.', 2),
-('Seppuku', '1962-01-01', 3, 'A samurai, Hanshiro Tsugumo, arrives at the house of the Iyi Clan with an unusual request. Due to his circumstances, he wishes to commit harakiri, ritual suicide, in the courtyard of the house. Turns out this is not such an unusual request: the clan has had such requests before, though the requestor''s intentions weren''t always honourable. The Clan elder tells Tsugumo the story of the last samurai who made the request and how that ended. Will he still go through with it?', 1),
-('Jusan-nin no shikaku', '1963-01-01', 2, 'A sadistic Daimyo (feudal lord) rapes a woman and murders both her and her husband, but even when one of his own vassals commits suicide to bring attention to the crime, the matter is quickly hushed up. Not only will there be no punishment, but because the Daimyo is the Shogun''s younger brother, he will soon be appointed to a high political position from which he could wreak even more havoc. Convinced that the fate of the Shogunate hangs in the balance, a plot is hatched to assassinate the Daimyo. The two most brilliant strategic minds of their generation find themselves pitted against each other; one is tasked to defend a man he despises, and has a small army at his disposal. The other is given a suicide mission, and has 12 brave men. They are the 13 Assassins.', 4),
-('The Unforgiven', '1960-01-01', 4, 'In post-Civil War Texas, the Zachary and Rawlins families are intrinsically tied together. Ben Zachary, the head of the family following the death of his father, Will Zachary, in Ben being the oldest son, and patriarch Zeb Rawlins are partners in a cattle ranching operation. Zeb''s unassuming son Charlie Rawlins would like to court Ben''s younger sister, adopted Rachel Zachary - something that doesn''t quite sit well with Ben in he believing Charlie not worthy of Rachel - while Zeb''s daughter Georgia Rawlins would be happy with any of the Zachary brothers, although she has her sights set on middle son Cash Zachary. Their world is upset with the arrival of an older man brandishing a saber. It is not the saber which is concerning, but rather the story that he is spreading: that Rachel is of Indian heritage, most specifically Kiowa. Regardless of the truth, which matriarch Mattilda Zachary knows, this story places a strain on the community who don''t want a "dirty Injun" in their midst, threatens all the relationships between the Zacharys and Rawlins, and causes a division within the Zachary family, as the Zacharys try to protect themselves and their property against those who don''t want Rachel there, including the Kiowa who want Rachel, one of their own, back.', 2),
-('The Sheepman', '1958-01-01', 5, 'A stranger in a Western cattle-town behaves with remarkable self-assurance, establishing himself as a man to be reckoned with. The reason appears with his stock: a herd of sheep, which he intends to graze on the range. The horrified inhabitants decide to run him out at all costs.', 3),
-('Per un pugno di dollari', '1964-01-01', 6, 'With nothing but profit on his mind, a taciturn American with a lightning-fast right hand rides into San Miguel, a sun-bleached border town where everyone is either rich or dead. As the amoral man with no name discovers that the sleepy town provides the perfect opportunity to make a quick buck, the mysterious stranger soon finds himself caught in the middle of a destructive, long-standing feud. After all, the corrupt Baxters and the murderous Rojos, two feuding families fighting for control, won''t let a nameless nobody get a slice of the cake. But the dusty, poncho-clad newcomer has already set his sights on the grand prize--the only way to get it is to play the gangs off against each other in a deadly battle of wits. Of course, no one has pulled such a reckless stunt and lived to tell the tale. How far is a man willing to go for a fistful of dollars?', 5);
+('Yojimbo', '1961', 1, 'asd', 1),
+('Shichinin no samurai', '1954', 1, 'A veteran samurai, who has fallen on hard times, answers a village''s request for protection from bandits. He gathers 6 other samurai to help him, and they teach the townspeople how to defend themselves, and they supply the samurai with three small meals a day. The film culminates in a giant battle when 40 bandits attack the village.', 2),
+('Seppuku', '1962', 3, 'A samurai, Hanshiro Tsugumo, arrives at the house of the Iyi Clan with an unusual request. Due to his circumstances, he wishes to commit harakiri, ritual suicide, in the courtyard of the house. Turns out this is not such an unusual request: the clan has had such requests before, though the requestor''s intentions weren''t always honourable. The Clan elder tells Tsugumo the story of the last samurai who made the request and how that ended. Will he still go through with it?', 1),
+('Jusan-nin no shikaku', '1963', 2, 'A sadistic Daimyo (feudal lord) rapes a woman and murders both her and her husband, but even when one of his own vassals commits suicide to bring attention to the crime, the matter is quickly hushed up. Not only will there be no punishment, but because the Daimyo is the Shogun''s younger brother, he will soon be appointed to a high political position from which he could wreak even more havoc. Convinced that the fate of the Shogunate hangs in the balance, a plot is hatched to assassinate the Daimyo. The two most brilliant strategic minds of their generation find themselves pitted against each other; one is tasked to defend a man he despises, and has a small army at his disposal. The other is given a suicide mission, and has 12 brave men. They are the 13 Assassins.', 4),
+('The Unforgiven', '1960', 4, 'In post-Civil War Texas, the Zachary and Rawlins families are intrinsically tied together. Ben Zachary, the head of the family following the death of his father, Will Zachary, in Ben being the oldest son, and patriarch Zeb Rawlins are partners in a cattle ranching operation. Zeb''s unassuming son Charlie Rawlins would like to court Ben''s younger sister, adopted Rachel Zachary - something that doesn''t quite sit well with Ben in he believing Charlie not worthy of Rachel - while Zeb''s daughter Georgia Rawlins would be happy with any of the Zachary brothers, although she has her sights set on middle son Cash Zachary. Their world is upset with the arrival of an older man brandishing a saber. It is not the saber which is concerning, but rather the story that he is spreading: that Rachel is of Indian heritage, most specifically Kiowa. Regardless of the truth, which matriarch Mattilda Zachary knows, this story places a strain on the community who don''t want a "dirty Injun" in their midst, threatens all the relationships between the Zacharys and Rawlins, and causes a division within the Zachary family, as the Zacharys try to protect themselves and their property against those who don''t want Rachel there, including the Kiowa who want Rachel, one of their own, back.', 2),
+('The Sheepman', '1958', 5, 'A stranger in a Western cattle-town behaves with remarkable self-assurance, establishing himself as a man to be reckoned with. The reason appears with his stock: a herd of sheep, which he intends to graze on the range. The horrified inhabitants decide to run him out at all costs.', 3),
+('Per un pugno di dollari', '1964', 6, 'With nothing but profit on his mind, a taciturn American with a lightning-fast right hand rides into San Miguel, a sun-bleached border town where everyone is either rich or dead. As the amoral man with no name discovers that the sleepy town provides the perfect opportunity to make a quick buck, the mysterious stranger soon finds himself caught in the middle of a destructive, long-standing feud. After all, the corrupt Baxters and the murderous Rojos, two feuding families fighting for control, won''t let a nameless nobody get a slice of the cake. But the dusty, poncho-clad newcomer has already set his sights on the grand prize--the only way to get it is to play the gangs off against each other in a deadly battle of wits. Of course, no one has pulled such a reckless stunt and lived to tell the tale. How far is a man willing to go for a fistful of dollars?', 5);
 
 insert into genre_movie (movie_id, genre_id)
 values
@@ -165,11 +161,9 @@ values
 select m.movie_title, m.movie_copies - count(r.movie_id) filter (where r.rental_returned isnull) as disponible from movie m left join rental r on m.movie_id = r.movie_id group by m.movie_id, m.movie_title, m.movie_copies order by m.movie_id;
 
 -- Numero de socio, nombre y Género favorito
-/* select customer_id as n_socio, c.customer_name, (select TOP 1 genre_id from genre_movie) as fav_genre from customer c
- * left join movie m on c.customer_id = m.movie_id
- * join genre_movie gm on m.movie_id = gm.movie_id
- * join genre g on g.genre_id = gm.genre_id
- * group by gm.genre_id, c.customer_id
- * order by fav_genre DESC;
- */
-
+select customer_id as n_socio, c.customer_name, (select count(genre_id) from genre_movie) as fav_genre from customer c 
+left join movie m on c.customer_id = m.movie_id 
+join genre_movie gm on m.movie_id = gm.movie_id
+join genre g on g.genre_id = gm.genre_id
+group by gm.genre_id, c.customer_id
+order by fav_genre DESC;
